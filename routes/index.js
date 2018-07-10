da80ae1b32610460155f7460052775a3ecf8c166
jeet bafna <jeetanil97@gmail.com>
2018-07-10 13:33:23 +0530

    mlab

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ var db = require('monk')('mongodb://jeetbafna:password1@ds231991.mlab.com:31991/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var db = req.db;
 	var posts = db.get('posts');
 	posts.find({},{},function(err,posts){
 		res.render('index', { posts: posts });
